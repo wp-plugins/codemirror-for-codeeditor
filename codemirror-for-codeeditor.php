c82facd4fd00cd3c3ffa3f27950df450f91f8c92
redcocker <redcocker@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-10-03 03:12:29 +0000

    release 0.2
    git-svn-id: https://plugins.svn.wordpress.org/codemirror-for-codeeditor/trunk@446535 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- codemirror-for-codeeditor.php
+++ codemirror-for-codeeditor.php
@@ -2,15 +2,15 @@
 /*
 Plugin Name: CodeMirror for CodeEditor
 Plugin URI: http://www.near-mint.com/blog/software/codemirror-for-codeeditor
-Description: Just another code syntaxhighligher for theme and plugn editor with CodeMirror.
-Version: 0.1
+Description: Just another code syntaxhighligher for the theme and plugin editor with CodeMirror. This plugin can highlight sourcecodes in theme/plugin editor and provide a useful toolbar.
+Version: 0.2
 Author: redcocker
 Author URI: http://www.near-mint.com/blog/
 Text Domain: cfc_lang
 Domain Path: /languages
 */
 /* 
-Last modified: 2011/9/30
+Last modified: 2011/10/3
 License: GPL v2(Except "CodeMirror" libraries)
 */
 /*  Copyright 2011 M. Sumitomo
@@ -35,7 +35,7 @@ License: GPL v2(Except "CodeMirror" libraries)
 
 load_plugin_textdomain('cfc_lang', false, dirname(plugin_basename(__FILE__)).'/languages');
 $cfc_plugin_url = plugin_dir_url(__FILE__);
-$cfc_ver = "0.1";
+$cfc_ver = "0.2";
 $cfc_theme = "default"; // Other themes: cobalt, eclipse, elegan, neat, night
 
 add_action('admin_print_styles-theme-editor.php', 'cfc_load_style');
@@ -51,6 +51,7 @@ function cfc_load_style() {
 	wp_enqueue_style('codemirror-theme-elegant',  $cfc_plugin_url.'codemirror/theme/elegant.css', false, '2.15');
 	wp_enqueue_style('codemirror-theme-neat',  $cfc_plugin_url.'codemirror/theme/neat.css', false, '2.15');
 	wp_enqueue_style('codemirror-theme-night',  $cfc_plugin_url.'codemirror/theme/night.css', false, '2.15');
+	wp_enqueue_style('codemirror-fullscreen',  $cfc_plugin_url.'css/dummy.css', false, '2.15');
 }
 
 add_action('admin_print_scripts-theme-editor.php', 'cfc_load_script');
@@ -67,6 +68,7 @@ function cfc_load_script() {
 	wp_enqueue_script('codemirror-mode-js', $cfc_plugin_url.'codemirror/mode/javascript/javascript.js', false, '2.15');
 	wp_enqueue_script('codemirror-mode-php', $cfc_plugin_url.'codemirror/mode/php/php.js', false, '2.15');
 	wp_enqueue_script('codemirror-mode-xml', $cfc_plugin_url.'codemirror/mode/xml/xml.js', false, '2.15');
+	wp_enqueue_script('codemirror-auto-complete',  $cfc_plugin_url.'codemirror/lib/complete.js', false, '2.15');
 }
 
 // Add scripts into the footer
@@ -74,7 +76,7 @@ add_action('admin_footer-theme-editor.php', 'cfc_run_script');
 add_action('admin_footer-plugin-editor.php', 'cfc_run_script');
 
 function cfc_run_script() {
-	global $cfc_theme, $pagenow;
+	global $cfc_plugin_url, $cfc_ver, $cfc_theme, $pagenow;
 	if (isset($_GET['file'])) {
 		$extension = strtolower(pathinfo(esc_html($_GET['file']), PATHINFO_EXTENSION));
 	} else {
@@ -84,7 +86,8 @@ function cfc_run_script() {
 			$extension = "php";
 		}
 	}
-	echo "\n<script type=\"text/javascript\">
+	echo "\n<!-- CodeMirror for CodeEditor Ver.".$cfc_ver." Scripts for CodeMirror Begin -->
+<script type=\"text/javascript\">
 var editor = CodeMirror.fromTextArea(document.getElementById(\"newcontent\"), {
 	theme: \"".$cfc_theme."\",
 	lineNumbers: true,
@@ -108,36 +111,129 @@ var editor = CodeMirror.fromTextArea(document.getElementById(\"newcontent\"), {
 			e.stop();
 			return toggleFullscreenEditing();
 		}
-	}
+		// Hook into ctrl-space
+		if (e.keyCode == 32 && (e.ctrlKey || e.metaKey) && !e.altKey) {
+			e.stop();
+			var editorDiv = jQuery('.CodeMirror-scroll');
+			if (!editorDiv.hasClass('fullscreen')) {
+				return startComplete();
+			}
+		}
+	},
 });
+
 function selectTheme(node) {
 	var theme = node.options[node.selectedIndex].innerHTML;
+	var editorDiv = jQuery('.CodeMirror-scroll');
 	editor.setOption(\"theme\", theme);
+	if (editorDiv.hasClass('fullscreen')) {
+		var css_file;
+		if (theme == 'cobalt') {
+			css_file = '".$cfc_plugin_url."css/cobalt.css';
+		} else if (theme == 'night') {
+			css_file = '".$cfc_plugin_url."css/night.css';
+		} else {
+			css_file = '".$cfc_plugin_url."css/white.css';
+		}
+		document.getElementById('codemirror-fullscreen-css').href = css_file;
+	}
 }
+
 function toggleFullscreenEditing(){
 	var editorDiv = jQuery('.CodeMirror-scroll');
-	var toolbarDiv = jQuery('.ace');
+	var toolbarDiv = jQuery('#cfc-toolbar');
 	if (!editorDiv.hasClass('fullscreen')) {
+		var selected_theme = document.getElementById(\"cfc-theme\").value;
+		var css_file;
+		if (selected_theme == 'cobalt') {
+			css_file = '".$cfc_plugin_url."css/cobalt.css';
+		} else if (selected_theme == 'night') {
+			css_file = '".$cfc_plugin_url."css/night.css';
+		} else {
+			css_file = '".$cfc_plugin_url."css/white.css';
+		}
+		document.getElementById('codemirror-fullscreen-css').href = css_file;
 		toggleFullscreenEditing.beforeFullscreen = { height: editorDiv.height(), width: editorDiv.width() }
 		editorDiv.addClass('fullscreen');
-		editorDiv.height('95%');
+		editorDiv.height('89%');
 		editorDiv.width('100%');
-		toolbarDiv.addClass('ace_ToolBar');
+		toolbarDiv.addClass('cfc-toolbar-full');
 		editor.refresh();
 	} else {
+		document.getElementById('codemirror-fullscreen-css').href = '".$cfc_plugin_url."css/dummy.css';
 		editorDiv.removeClass('fullscreen');
-		toolbarDiv.removeClass('ace_ToolBar');
+		toolbarDiv.removeClass('cfc-toolbar-full');
 		editorDiv.height(toggleFullscreenEditing.beforeFullscreen.height);
 		editorDiv.width(toggleFullscreenEditing.beforeFullscreen.width);
 		editor.refresh();
 	}
 }
-</script>\n";
+</script>";
 
+echo "\n<!-- CodeMirror for CodeEditor Ver.".$cfc_ver." Scripts for CodeMirror End -->
+<!-- CodeMirror for CodeEditor Ver.".$cfc_ver." Scripts for toolbar Begin -->\n";
 ?>
 <script type="text/javascript">
-jQuery("#newcontent").after("<div id =\"cfc-selecter\"><label><?php _e("Select a theme: ", "cfc_lang") ?></label><select id=\"cfc-theme\" onchange=\"selectTheme(this)\"><option>default</option><option>cobalt</option><option>eclipse</option><option>elegant</option><option>neat</option><option>night</option></select></div>");
+var lastPos = null, lastQuery = null, marked = [];
+
+function unmark() {
+	for (var i = 0; i < marked.length; ++i) marked[i]();
+	marked.length = 0;
+}
+
+function search() {
+	unmark();                     
+	var text = document.getElementById("query").value;
+	if (!text) return;
+	for (var cursor = editor.getSearchCursor(text); cursor.findNext();)
+		marked.push(editor.markText(cursor.from(), cursor.to(), "searched"));
+
+	if (lastQuery != text) lastPos = null;
+	var cursor = editor.getSearchCursor(text, lastPos || editor.getCursor());
+	if (!cursor.findNext()) {
+		cursor = editor.getSearchCursor(text);
+		if (!cursor.findNext()) return;
+	}
+	editor.setSelection(cursor.from(), cursor.to());
+	lastQuery = text; lastPos = cursor.to();
+}
+
+function replace() {
+	unmark();
+	var text = document.getElementById("query").value,
+	replace = document.getElementById("replace_str").value;
+	if (!text) return;
+	var cursor = editor.getSearchCursor(text);
+	cursor.findNext();
+	if (!cursor) return;
+	editor.replaceRange(replace, cursor.from(), cursor.to());
+	editor.setSelection(cursor.from(), cursor.to());
+}
+
+function replace_all() {
+	unmark();
+	var text = document.getElementById("query").value,
+	  replace = document.getElementById("replace_str").value;
+	if (!text) return;
+	for (var cursor = editor.getSearchCursor(text); cursor.findNext();)
+		cursor.replace(replace);
+}
+
+function clear_result(){
+	lastQuery = null;
+	lastPos = null;
+	unmark();
+	document.getElementById("query").value = '';
+	document.getElementById("replace_str").value = '';
+}
 </script>
+<?php echo "<!-- CodeMirror for CodeEditor Ver.".$cfc_ver." Scripts for toolbar End -->
+<!-- CodeMirror for CodeEditor Ver.".$cfc_ver." Scripts for creating toolbar Begin -->\n"; ?>
+<script type="text/javascript">
+jQuery("#newcontent").after("<div id =\"cfc-toolbar\"><label><?php _e("Theme: ", "cfc_lang") ?></label><select id=\"cfc-theme\" onchange=\"selectTheme(this)\"><option value=\"default\">default</option><option value=\"cobalt\">cobalt</option><option value=\"eclipse\">eclipse</option><option value=\"elegant\">elegant</option><option value=\"neat\">neat</option><option value=\"night\">night</option></select> <input type=\"text\" size=\"12\" id=\"query\" /><button type=\"button\" onclick=\"search()\"><?php _e("Search", "cfc_lang") ?></button> with <input type=\"text\" size=\"12\" id=\"replace_str\" /><button type=\"button\" onclick=\"replace()\"><?php _e("Replace", "cfc_lang") ?></button> <button type=\"button\" onclick=\"replace_all()\"><?php _e("Replace All", "cfc_lang") ?></button> <button type=\"button\" onclick=\"clear_result()\"><?php _e("Clear", "cfc_lang") ?></button></div>");
+</script>
+<?php echo "<!-- CodeMirror for CodeEditor Ver.".$cfc_ver." Scripts for creating toolbar End -->\n"; ?>
+
 <?php }
 
 // Add addtional style into the header
@@ -146,16 +242,19 @@ add_action('admin_head-plugin-editor.php', 'cfc_additional_style');
 
 function cfc_additional_style() {
 	global $cfc_ver;
-	echo "\n<!-- CodeMirror for CodeEditor Ver.".$cfc_ver." CSS Begin -->\n";
-	echo "<style>
+	echo "\n<!-- CodeMirror for CodeEditor Ver.".$cfc_ver." Differential css Begin -->\n";
+	echo "<style type=\"text/css\">
 .CodeMirror-scroll {height: 600px;overflow: auto; margin-right: 0 !important;}
-.CodeMirror-gutter {width: 35px !important;}
+.CodeMirror-gutter {width: 45px !important;}
 #template div {margin-right: 105px;}
-.activeline {background: #f0fcff !important;}
-.fullscreen{background-color: #FFFFFF; height: 95%; right: 0;position: fixed; top: 40px; width: 100%; z-index: 100;}
-#cfc-selecter {margin-bottom: 2px}
-</style>\n";
-	echo "<!-- CodeMirror for CodeEditor Ver.".$cfc_ver." CSS End -->\n";
+.searched {background: yellow;}
+.fullscreen {height: 89%; right: 0;position: fixed; top: 80px; width: 100%; z-index: 100;}
+#cfc-toolbar {margin-bottom: 2px;}
+.cfc-toolbar-full {background-color: #ffffff; min-height: 85px; position: fixed; top: 50px; z-index: 100;}
+.completions {position: absolute; z-index: 10; overflow: hidden; -webkit-box-shadow: 2px 3px 5px rgba(0,0,0,.2); -moz-box-shadow: 2px 3px 5px rgba(0,0,0,.2); box-shadow: 2px 3px 5px rgba(0,0,0,.2);}
+.completions select {background: #fafafa; outline: none; border: none; padding: 0; margin: 0; font-family: monospace;}
+</style>";
+	echo "\n<!-- CodeMirror for CodeEditor Ver.".$cfc_ver." Differential css End -->\n";
 }
 
 ?>
--- readme.txt
+++ readme.txt
@@ -4,21 +4,33 @@ Donate link: http://www.near-mint.com/blog/donate
 Tags: CodeMirror, syntaxhighlighter, sourcecode, code, syntax, highlight, highlighting, editor, plugin, theme
 Requires at least: 2.8
 Tested up to: 3.2.1
-Stable tag: 0.1
+Stable tag: 0.2
 
-Just another code syntaxhighligher for theme and plugn editor with CodeMirror.
+Just another code syntaxhighligher for the theme and plugin editor with CodeMirror.
 
 == Description ==
 
-Just another code syntaxhighligher for theme and plugn editor with CodeMirror.
-
-Support PHP, Javascript, HTML, CSS.
+Just another code syntaxhighligher for the theme and plugin editor with CodeMirror. This plugin can highlight sourcecodes in the theme/plugin editor and provide a useful toolbar.
 
 = Features =
 
 * Highlight sourcecodes in theme and plugin editor on the dashboard.
 * Based on Marijn Haverbeke's "[CodeMirror](http://codemirror.net/ "CodeMirrorr")" JavaScript library.
+* Built-in 6 themes.
+* Useful toolbar which includes search/replace features.
+* Full-screen editing.
+* Auto-complete.
 * Localization: English(Default), 日本語(Japanese, UTF-8).
+
+= Support languages =
+* PHP
+* CSS
+* Javascript
+* (X)HTML
+
+= Recommended plugin =
+
+* "[WP SyntaxHighlighter](http://wordpress.org/extend/plugins/wp-syntaxhighlighter/ "WP SyntaxHighlighter")" can highlight sourcecodes on your front-end(posts, pages, comments).
 
 == Installation ==
 
@@ -29,22 +41,30 @@ Support PHP, Javascript, HTML, CSS.
 
 = Usage =
 
-You can select a theme in the drop-down menu.
+* You can select a theme, search and replace on the toolbar.
+* Place the cursor in the textarea and press "Esc" or "F11" key to chage to the "fullscreen mode".
+* Press "Ctrl - Space" to activate Auto-omplete.
 
-After placing your cursor in the textarea, press "Esc" or "F11" key to chage to "fullscreen mode".
-
-Note: "cobalt" and "night" themes can't work correctly in "fullscreen mode".
+Note: Auto-omplete can't work in the "fullscreen mode".
 
 == Screenshots ==
 
-1. This is highlighted code.
+1. This is a highlighted code in the plugin editor.
 
 == Changelog ==
+
+= 0.2 =
+* Added Search and Replace functions.
+* Added Auto-complete.
+* The themes become user-selectable in the "fullscreen mode".
 
 = 0.1 =
 * This is the initial release.
 
 == Upgrade Notice ==
+
+= 0.2 =
+This version has some new features.
 
 = 0.1 =
 This is the initial release.
